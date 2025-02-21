1f111edb87f5e16980503d1cc1d8d87973889032
lu <mluxunqi@gmail.com>
2025-02-21 16:34:41 +0100

    Update spoj6.cpp

--- spoj6.cpp
+++ spoj6.cpp
@@ -8,7 +8,7 @@ string nextPalindrome(string k) {
     string first_half = k.substr(0, n / 2);
     string middle;
     if (n % 2 == 0) {
-        middle = "";
+    middle = "";
     } else {
         middle = k.substr(n / 2, 1);
     }
@@ -16,7 +16,7 @@ string nextPalindrome(string k) {
     for (int i = first_half.size() - 1; i >= 0; --i) {
         second_half += first_half[i];
     }
-    string palindrome = first_half + middle + second_half;
+        string palindrome = first_half + middle + second_half;
     if (palindrome > k) {
         return palindrome;
     }
@@ -25,21 +25,22 @@ string nextPalindrome(string k) {
             palindrome[i]++;
             if (i != n - i - 1) {
                 palindrome[n - i - 1] = palindrome[i];
-            }
+             }
             return palindrome;
         }
+        palindrome[i] = palindrome[n - i - 1] = '0';
     }
-    palindrome[i] = palindrome[n - i - 1] = '0';
+
     return "1" + string(n - 1, '0') + "1";
 }
 
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string k;
-        cin >> k;
-        cout << nextPalindrome(k) << endl;
-    }
+ int t;
+ cin >> t;
+ while (t--) {
+    string k;
+    cin >> k;
+    cout << nextPalindrome(k) << endl;
+ }
     return 0;
 }
